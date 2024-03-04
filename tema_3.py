e00632f414f296833d7362933a2c928d1f5283c5
MariaGargoles <mariagargoles@live.com>
2024-03-04 11:24:07 +0100

    pequeña correccion

--- tema_3.py
+++ tema_3.py
@@ -1,44 +1,45 @@
-#Funciones
-#se crean usando la palabra sereveada def
+# Funciones
+# Se crean usando la palabra reservada def
 # def NOMBRE_FUNCION(PARAM_1, PARAM_2, ..)
-#La funcion mas sencilla, que no hace nada
+# La función más sencilla, que no hace nada
 def foo():
     pass
 
 print(type(foo))
 
-#Las funciones pueden llevar una cadena de documentacion, que NO es un comentario
-#en esta cadena de documentacion se suele ncluir una descripcion de la funcion
-# y un resumen de los parametros de entrada y salida
-#la diferencia con los comenarios es que se pueden consultar
-#invocando al metodo magico __doc__ que nos dara acceso a la documentaicon de la funcion
+# Las funciones pueden llevar una cadena de documentación, que NO es un comentario
+# En esta cadena de documentación se suele incluir una descripción de la función
+# y un resumen de los parámetros de entrada y salida
+# La diferencia con los comentarios es que se pueden consultar
+# invocando al método mágico __doc__ que nos dará acceso a la documentación de la función
 def foo():
     """
-        Ejemplo de cadena de documentacion de la funcion que no hace nada
-        Parametros de entrada:
-            ninguno
-        Salida:
-            ninguna
+    Ejemplo de cadena de documentación de la función que no hace nada
+    Parametros de entrada:
+        ninguno
+    Salida:
+        ninguna
     """
     pass
 
 print(foo.__doc__)
 
-#Las propias de python todas tienen documentacion
+
+# Las propias de python todas tienen documentación
 print(print.__doc__)
 print(max.__doc__)
 
-#Las fuciones pueden tener un cuerpo que son las operaciones que se realizan dentro de la funcion
+# Las funciones pueden tener un cuerpo que son las operaciones que se realizan dentro de la función
 def suma():
     a = 10
     b = 5
     print(a + b)
 
-#Las funciones se deben invocar, es el momento de la invocacion cuando se ejecuta el codigo asociado
+# Las funciones se deben invocar, es el momento de la invocación cuando se ejecuta el código asociado
 suma()
 
-#Las funciones devuelve SIEMPRE, la diferencia es que con la sentencia return definimos que queremos devolver
-#si no la indicamos por defecto devuelve un None
+# Las funciones devuelven SIEMPRE, la diferencia es que con la sentencia return definimos qué queremos devolver
+# si no la indicamos por defecto devuelve un None
 
 def foo():
     a = 10
@@ -46,7 +47,7 @@ def foo():
 retorno = foo()
 print(retorno)
 
-#Si le ponemos el return definimos que queremos devolver
+# Si le ponemos el return definimos qué queremos devolver
 def foo():
     a = 10
     return a
@@ -54,128 +55,128 @@ def foo():
 retorno = foo()
 print(retorno)
 
-#En python las funciones pueden devolver multiples valores, simplemente debemos separarlos por comas en
-#el return
+# En python, las funciones pueden devolver múltiples valores, simplemente debemos separarlos por comas en
+# el return
 
 def foo():
     a = 10
     b = 20
-    return a,b
+    return a, b
     
 retorno = foo()
 print(retorno)
 print(type(retorno)) 
 
-#en el fondo foo devuelve un valor, lo que hace es juntarlos todos en una tupla
-#podemos realizar asignaciones con el packing y unpaking
+# En el fondo, foo devuelve un valor; lo que hace es juntarlos todos en una tupla
+# Podemos realizar asignaciones con el packing y unpacking
 def foo():
     a = 10
     b = 20
-    return a,b
+    return a, b
     
 retorno_1, retorno_2 = foo()
 print(retorno_1, retorno_2)
 
-#Las funciones pueden recibir argumentos, que pueden ser de cualquier tipo
-def foo(x,y):
-    print(x,y)
 
-#en el momento de la invocacion debemos asignar valores
-foo(19,90)
+# Las funciones pueden recibir argumentos, que pueden ser de cualquier tipo
+def foo(x, y):
+    print(x, y)
 
-#Por defecto los parametros de las funciones en python, son POSICIONALES y OBLIGATORIOS
-def foo(x,y):
-    print(x,y)
+# En el momento de la invocación, debemos asignar valores
+foo(19, 90)
 
-foo()
+# Por defecto, los parámetros de las funciones en python son POSICIONALES y OBLIGATORIOS
+def foo(x, y):
+    print(x, y)
+
+foo()  # Esto dará error ya que faltan argumentos
 foo(90)
 
-#Podemos trabajar con parametros optativos
-#en la firma de la funcion le debemos indicar los valores por efecto de los parametros
-#solo se aplican si no viene un valor como argumento
-#ejemplo de parametro y como optativo
-def foo(x,y=10):
-    print(x,y)
+# Podemos trabajar con parámetros opcionales
+# En la firma de la función le debemos indicar los valores por defecto de los parámetros
+# Solo se aplican si no viene un valor como argumento
+# Ejemplo de parámetro y como optativo
+def foo(x, y=10):
+    print(x, y)
 
 foo(1)
-foo(1,2)
+foo(1, 2)
 
-#Importante los parametros optativos siempre deben ir al final de la firma
-def foo(x=10,y):
-    print(x,y)
+# Importante: los parámetros opcionales siempre deben ir al final de la firma
+def foo(x=10, y):
+    print(x, y)  # Esto dará error ya que x tiene un valor por defecto y viene después de y
 
-#Podemos tener tantos argumentos optativos como queramos
-def foo(x=20,y=10):
-    print(x,y)
+# Podemos tener tantos argumentos opcionales como queramos
+def foo(x=20, y=10):
+    print(x, y)
 
 foo()
 foo(1)
-foo(1,2)
+foo(1, 2)
 
-#Los parametros tambien se le puede modificar el tema posicionalidad
-#en esta caso diriamos que trabajamos con parametros Nominales
-#se definen en el moento de la invocaicon, y deben coincidir con el identificador
-#deinido en l firma de la funcion
+# Los parámetros también se le pueden modificar el tema posicionalidad
+# En este caso, diríamos que trabajamos con parámetros Nominales
+# Se definen en el momento de la invocación y deben coincidir con el identificador
+# definido en la firma de la función
 
-def foo(x,y):
-    print(x,y)
+def foo(x, y):
+    print(x, y)
 
-#Nominales
-    
-foo(y = 10, x = 20)
 
-#Debemos respetar la misma regla que con los optativos, los nominales al final de la invocacion
+# Nominales
+foo(y=10, x=20)
 
-def foo(x,y):
-    print(x,y)
+# Debemos respetar la misma regla que con los opcionales, los nominales al final de la invocación
 
-#Nominales
+def foo(x, y):
+    print(x, y)
 
-foo(y = 10, x)
+# Nominales
+foo(y=10, x)
 
-#Se pueden combinar ambas ideas
-def foo(x,y=100,z=20):
-    print(x,y,z)
+# Se pueden combinar ambas ideas
+def foo(x, y=100, z=20):
+    print(x, y, z)
 
 foo(1)
-foo(z=10,x=10)
-foo(z=10,x=10,y=100)
-foo(1,2,4)
+foo(z=10, x=10)
+foo(z=10, x=10, y=100)
+foo(1, 2, 4)
 
-#Ademas de esto, en python las funciones pueden recibir un numero arbitrario de parametros
-#como por ejemplo la funcion print
-#en este caso en la firma de la funcion debemos indicarlo con el operador *
+# Además de esto, en python las funciones pueden recibir un número arbitrario de parámetros
+# como por ejemplo la función print
+# en este caso en la firma de la función debemos indicarlo con el operador *
 def foo(*parametros):
     print(parametros)
     print(type(parametros))
 
 foo()
 foo(1)
-foo(1,2)
-foo(1,2,3)
-foo(1,2,3,4)
+foo(1, 2)
+foo(1, 2, 3)
+foo(1, 2, 3, 4)
 
-#Tambien podemos tener un numero arbitrario de parametros nominales, para ello en la firma
-#usarmeos un **
+# También podemos tener un número arbitrario de parámetros nominales, para ello en la firma
+# usaremos un **
 def foo(**parametros):
     print(parametros)
     print(type(parametros))
 
 foo(x=10)
-foo(x=10,y=10)
-foo(x=10,z=20,y=987)
+foo(x=10, y=10)
+foo(x=10, z=20, y=987)
 
-#Podemos combinar todo lo anterior
-def foo(x,y=100,z=20, *posicionales, **nominales):
-    print(x,y,z)
+# Podemos combinar todo lo anterior
+def foo(x, y=100, z=20, *posicionales, **nominales):
+    print(x, y, z)
     print(posicionales)
     print(nominales)
 
 foo(1)
-foo(1,2)
-foo(1,2,3)
-foo(1,2,3,2,3,4,5,6)
-foo(1,2,3,4,g=10,h=19)
+foo(1, 2)
+foo(1, 2, 3)
+foo(1, 2, 3, 2, 3, 4, 5, 6)
+foo(1, 2, 3, 4, g=10, h=19)
 
 # Funciones recursivas
 # Son funciones que se invocan a sí mismas
@@ -203,13 +204,12 @@ def contar(numero):
 
 contar(10)
 
-#Factorial en recursivo
+# Factorial en recursivo
 def factorial(numero):
     if numero == 1:
         return 1
     else:
         return factorial(numero - 1) * numero
-
 
 print(factorial(5))
 
@@ -259,22 +259,22 @@ print(fibonacci(6)) #devolver 13
 #fibonacci(5) = 5 + 3 = 8
 #fibonacci(6) = 8 + 5 = 13
 
-#Alcances de variables en las funciones
+# Alcances de variables en las funciones
 # tenemos 2 tipos de variables las globales y las locales
-# ademas de las que pasamos por parametro
-#Alcance variables de parametro
-# por valor y por referencia, siendo el primero una copia mientrasque por referencia es la misma variable
+# además de las que pasamos por parámetro
+# Alcance variables de parámetro
+# por valor y por referencia, siendo el primero una copia mientras que por referencia es la misma variable
+
 def foo(x):
     print(x)
-    #Por defecto es una copia y los cambios no le afectan
+    # Por defecto es una copia y los cambios no le afectan
     x = 10
     print(x)
 
 x = 100
 foo(x)
-#aqui sigue valiendo 100
+# aquí sigue valiendo 100
 print(x)
-
 
 def cuadrados(lista):
     retorno = []
@@ -283,48 +283,48 @@ def cuadrados(lista):
     lista = retorno
     print(lista)
 
-lista = [1,2,3]
+lista = [1, 2, 3]
 print(lista)
 cuadrados(lista)
 print(lista)
 
-#Otro ejemplo 
-#OJO con lo de paso por valor y referencia
-#las variables INMUTABLES (enteros, flotantes, complejos, tuplas, string, boolean)las pasa como una copia
-#Las variables MUTABLES (listas, diccionarios, conjuntos), pasa la propia variable
+# Otro ejemplo
+# OJO con lo de paso por valor y referencia
+# las variables INMUTABLES (enteros, flotantes, complejos, tuplas, string, boolean) las pasa como una copia
+# Las variables MUTABLES (listas, diccionarios, conjuntos), pasa la propia variable
 def suma(lista):
     for posicion in range(len(lista)):
         lista[posicion] = lista[posicion] + 100
     print(lista)
 
-lista = [1,2,3]
+lista = [1, 2, 3]
 print(lista)
 suma(lista)
 print(lista)
 
-#OJo no tiene que ver con el identificador
+# OJo no tiene que ver con el identificador
 def suma(paco):
     for posicion in range(len(paco)):
         paco[posicion] = paco[posicion] + 100
     print(paco)
 
-lista = [1,2,3]
+lista = [1, 2, 3]
 print(lista)
 suma(lista)
 print(lista)
 
-#Globales vs locales
-#Una variable global es comun a todo el codigo
-#Una variable local solo tiene el alcance de su bloque
+# Globales vs locales
+# Una variable global es común a todo el código
+# Una variable local solo tiene el alcance de su bloque
 
 def foo():
     variable = 10
 
 foo()
-#Esta variable no existe, porque es local a la funcion
+# Esta variable no existe, porque es local a la función
 print(variable)
 
-#globales, son de solo lectura
+# globales, son de solo lectura
 variable = 100
 
 def foo():
@@ -333,32 +333,32 @@ def foo():
 foo()
 print(variable)
 
-#OJO son globales de solo lectura por defecto
+# OJO son globales de solo lectura por defecto
 variable = 100
 
 def foo():
-    #en el momento que creo una variable aun que tenga el mismo nombre que la global
-    #crea una nueva variable local a la funcion
+    # en el momento que creo una variable aunque tenga el mismo nombre que la global
+    # crea una nueva variable local a la función
     variable = 200
     print(variable)
 
 foo()
 print(variable)
 
-#Otro mas CUIDADO
+# Otro más CUIDADO
 variable = 100
 
 def foo():
-    #en el momento que ponemos la igualdad crea la variable y pierde el acceso a la blobal
+    # en el momento que ponemos la igualdad crea la variable y pierde el acceso a la global
     variable = variable + 200
     print(variable)
 
 foo()
 print(variable)
 
-#para poder modificar valores globales dentro de la funcion
-#tenemos la palabra reservada global
-#que debemos usar ANTES de hacer referencia a la funcion
+# para poder modificar valores globales dentro de la función
+# tenemos la palabra reservada global
+# que debemos usar ANTES de hacer referencia a la función
 variable = 100
 
 def foo():
@@ -369,7 +369,7 @@ def foo():
 foo()
 print(variable)
 
-#OJO si la variable no existe la crea
+# OJO si la variable no existe la crea
 def foo():
     global x
     x = 10000
@@ -377,7 +377,7 @@ def foo():
 foo()
 print(x)
 
-#Certificacion
+#! Preguntas certificación
 
 def foo():
     global x
@@ -394,5 +394,3 @@ foo()
 foo_2(x)
 foo_3()
 print(x)
-
-
